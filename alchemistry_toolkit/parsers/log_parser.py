8be0522413d5c3a2bcb702c623731ebc13a66b02
Wei-Tse Hsu <wehs7661@users.noreply.github.com>
2020-08-24 14:08:14 -0600

    Developed get_WL_data in log_parser.py and its unit test

--- alchemistry_toolkit/parsers/log_parser.py
+++ alchemistry_toolkit/parsers/log_parser.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy 
 
 def logger(output, *args, **kwargs):
     print(*args, **kwargs)
@@ -21,6 +22,7 @@ def set_params(obj, line, param_name, attr_name=None, dtype=float):
         attr_name = param_name
     if param_name in line and hasattr(obj, attr_name) is False:
         setattr(obj, attr_name, dtype(line.split('=')[1]))
+    
 
 class EXE_LogInfo:
 
@@ -53,9 +55,9 @@ class EXE_LogInfo:
 
             # parameters specific for EXE (deactivated in lambda-MetaD)
             set_params(self, l, 'weight-equil-wl-delta', 'cutoff')
-            set_params(self, l, 'init-wl-deta', 'init_wl')
-            set_params(self, l, 'wl-ratio')
-            set_params(self, l, 'wl-scale')
+            set_params(self, l, 'init-wl-delta', 'init_wl')
+            set_params(self, l, 'wl-ratio', 'wl_ratio')
+            set_params(self, l, 'wl-scale', 'wl_scale')
 
             # parameters specific for lambda-MetaD
             if 'PLUMED: Version: ' in l and hasattr(self, 'type') is False:
@@ -87,6 +89,11 @@ class EXE_LogInfo:
         """
         This function parses the log file and finds out the count and weight
         of each labmda state at the last time frame.
+
+        Returns
+        -------
+        final_counts (np.array): the counts of states at the last time frame
+        final_weights (np.array): the weights of states at the last time frame
         """
         f = open(self.input, 'r')
         lines = f.readlines()
@@ -124,8 +131,87 @@ class EXE_LogInfo:
             
         return final_counts, final_weights
     
-    def get_equil_data(self):
-        pass
+    def get_WL_data(self):
+        """
+        This function parses the log file and performs the following tasks:
+        1. Extract the data of WL incrementor as a function of time
+        2. Extract the data of free energy difference (between the coupled and 
+            uncoupled state) as a function of time
+        3. Assign equilibrated weights (if exist) as an attribute
+
+        Returns
+        -------
+        update_time (np.array):
+        delta_w (np.array)
+
+        """
+        f = open(self.input, 'r')
+        lines = f.readlines()
+        f.close()
+
+        delta_w = [self.init_wl]  # Wang-Landau incrementor \delta_w
+        step = [0]     # simulation steps at which the WL incrementor was updated
+        n_updates = 0
+        line_n = self.start
+
+        for l in lines[self.start:]: # skip the metadata
+            line_n += 1 
+
+            if 'weights are now:' in l:
+                if hasattr(self, 'EXE_status') is False:
+                    self.EXE_status = 'updating'
+                n_updates += 1
+                delta_w.append(np.round(self.init_wl * self.wl_scale ** n_updates, 7))
+                step.append(int(l.split(':')[0].split()[1]))
+
+            # Info about weights equilibration
+            if 'Weights have equilibrated' in l :
+                # Part 1: Basic information
+                self.EXE_status = 'equilibrated'
+                self.equil_c = []   # equilibrated counts
+                equil_step = l.split(':')[0].split()[1]
+                self.equil_t = np.round(float(equil_step) * self.dt / 1000, 5)  # units: ns
+
+                # Part 2: Search for equilibrated weights
+                search_lines = lines[line_n - 8 : line_n]  
+                for l_search in search_lines:
+                    if 'weights are now: ' in l_search:
+                        self.equil_w = l_search.split(':')[2]  # type: str
+
+                # Part 3: Search for equilibrated counts
+                search_lines = lines[line_n - (30 + self.N_states): line_n]  # for searching equilibrated counts
+                search_line_n = line_n - (30 + self.N_states)
+                for l_search in search_lines:
+                    search_line_n += 1
+                    if 'MC-lambda information' in l_search:  # lines[search_line_n - 1]
+                        print(lines[search_line_n - 1])
+                        for i in range(self.N_states):
+                            if lines[search_line_n + 2 + i].split()[-1] == '<<':
+                                self.equil_c.append(float(lines[search_line_n + 2 + i].split()[-4]))
+                            else:
+                                self.equil_c.append(float(lines[search_line_n + 2 + i].split()[-3]))
+                    
+                break 
+        
+        update_time = np.array(step) * self.dt / 1000  # time array for plotting (units :ns)
+        delta_w = np.array(delta_w)
+
+        return update_time, delta_w
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+        
 
         
 
--- alchemistry_toolkit/tests/test_log_parser.py
+++ alchemistry_toolkit/tests/test_log_parser.py
@@ -1,8 +1,12 @@
+import os
 import numpy as np
 import pytest
 import copy
 import alchemistry_toolkit
 from alchemistry_toolkit.parsers.log_parser import *
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+data_path = os.path.join(current_path, 'sample_data')
 
 file_lambda_MetaD = 'sample_files/lambda_MetaD.log'
 file_EXE_updating = 'sample_files/EXE_updating.log'
@@ -25,27 +29,26 @@ class Test_EXE_LogInfo:
          
     def test_init(self):
         expected_1 = {'init_w': [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                      'input': 'sample_files/lambda_MetaD.log',
-                      'dt': 0.002, 'nstlog': 1000, 'N_states': 9,
-                      'fixed': False, 'cutoff': 0.001, 'wl-scale': 0.8,
-                      'wl-ratio': 0.8, 'temp': 298.0, 'plumed_ver': '2.7.0-dev',
-                      'type': 'lambda-MetaD', 'start': 506}
+                      'input': 'sample_files/lambda_MetaD.log', 'dt': 0.002, 
+                      'nstlog': 1000, 'N_states': 9, 'fixed': False, 'cutoff': 0.001, 
+                      'wl_scale': 0.8, 'wl_ratio': 0.8, 'init_wl': 0.5, 'temp': 298.0, 
+                      'plumed_ver': '2.7.0-dev', 'type': 'lambda-MetaD', 'start': 506}
 
         # the metadata of EXE_upating.log and EXE_equilibrated.log are the same
         expected_2 = {'init_w': [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], 
                       'input': 'sample_files/EXE_updating.log', 'dt': 0.002, 
                       'nstlog': 1000.0, 'N_states': 9, 'fixed': False, 'cutoff': 0.001, 
-                      'wl-scale': 0.8, 'wl-ratio': 0.8, 'temp': 298.0, 'start': 455, 
-                      'type': 'expanded_ensemble'}   
+                      'wl_scale': 0.8, 'wl_ratio': 0.8, 'init_wl': 0.5, 'temp': 298.0, 
+                      'start': 455, 'type': 'expanded_ensemble'}   
         
         expected_3 = copy.deepcopy(expected_2)
         expected_3['input'] = 'sample_files/EXE_equilibrated.log'
 
         expected_4 = {'init_w': [0.0, 7.67256, 13.8818, 16.9028, 18.8082, 20.5498, 21.2318, 17.6905, 14.8862], 
-                      'input': 'sample_files/EXE_fixed.log', 
-                      'dt': 0.002, 'nstlog': 1000.0, 'N_states': 9, 
-                      'fixed': True, 'wl-scale': 0.8, 'wl-ratio': 0.8, 'temp': 298.0, 
-                      'start': 454, 'type': 'expanded_ensemble'}
+                      'input': 'sample_files/EXE_fixed.log', 'dt': 0.002, 
+                      'nstlog': 1000.0, 'N_states': 9, 'fixed': True, 'wl_scale': 0.8, 
+                      'wl_ratio': 0.8, 'init_wl': 1, 'temp': 298.0, 'start': 454, 
+                      'type': 'expanded_ensemble'}
 
         assert vars(test_1) == expected_1
         assert vars(test_2) == expected_2
@@ -62,18 +65,60 @@ class Test_EXE_LogInfo:
         c4 = np.array([35254, 29835, 28314, 31311, 28662, 34624, 37771, 13790, 10439])
         w4 = copy.deepcopy(w3)
 
-        assert (c1 == test_1.get_final_data()[0]).all()
-        assert (w1 == test_1.get_final_data()[1]).all()
-        assert (c2 == test_2.get_final_data()[0]).all()
-        assert (w2 == test_2.get_final_data()[1]).all()
-        assert (c3 == test_3.get_final_data()[0]).all()
-        assert (w3 == test_3.get_final_data()[1]).all()
-        assert (c4 == test_4.get_final_data()[0]).all()
-        assert (w4 == test_4.get_final_data()[1]).all()
+        np.testing.assert_array_almost_equal(c1, test_1.get_final_data()[0], 10)
+        np.testing.assert_array_almost_equal(w1, test_1.get_final_data()[1], 10)
+        np.testing.assert_array_almost_equal(c2, test_2.get_final_data()[0], 10)
+        np.testing.assert_array_almost_equal(w2, test_2.get_final_data()[1], 10)
+        np.testing.assert_array_almost_equal(c3, test_3.get_final_data()[0], 10)
+        np.testing.assert_array_almost_equal(w3, test_3.get_final_data()[1], 10)
+        np.testing.assert_array_almost_equal(c4, test_4.get_final_data()[0], 10)
+        np.testing.assert_array_almost_equal(w4, test_4.get_final_data()[1], 10)
 
         assert test_1.final_t == 5000
         assert test_2.final_t == 1000
         assert test_3.final_t == 5000
         assert test_4.final_t == 5000
+
+        assert test_2.EXE_status == 'updating'
+
+    
+    def test_get_WL_data(self):
+        # Test 1: EXE_updating
+        t1 = np.array([0, 0.01758, 0.02574, 0.04432, 0.062, 0.07464, 0.10316, 
+                       0.12102, 0.1402, 0.1864, 0.24034, 0.26614, 0.31396, 
+                       0.32938, 0.42008, 0.5014, 0.56918, 0.6576, 0.7434, 0.9499])
+        w1 = np.array([0.5, 0.4, 0.32, 0.256, 0.2048, 0.16384, 0.131072 , 0.1048576, 
+                       0.0838861, 0.0671089, 0.0536871, 0.0429497, 0.0343597, 
+                       0.0274878, 0.0219902, 0.0175922, 0.0140737, 0.011259 ,
+                       0.0090072, 0.0072058])
+
+        np.testing.assert_array_almost_equal(t1, test_2.get_WL_data()[0], 10)
+        np.testing.assert_array_almost_equal(w1, test_2.get_WL_data()[1], 10)
+        assert test_2.EXE_status == 'updating'
+
+        # Test 2: EXE_equilibrated
+        t2 = np.array([0, 0.01498, 0.02138, 0.0353 , 0.0536 , 0.07438, 0.09064,
+                       0.11154, 0.13876, 0.18484, 0.21016, 0.26386, 0.31684, 0.33002,
+                       0.3672, 0.43502, 0.50318, 0.58672, 0.67496, 0.7618 , 0.89586,
+                       0.9931, 1.175, 1.27582, 1.57056, 1.66066, 1.90268, 2.19516,
+                       2.87514])
+        w2 = np.array([0.5, 0.4, 0.32, 0.256, 0.2048, 0.16384, 0.131072 , 0.1048576, 
+                       0.0838861, 0.0671089, 0.0536871, 0.0429497, 0.0343597, 0.0274878, 
+                       0.0219902, 0.0175922, 0.0140737, 0.011259, 0.0090072, 0.0072058, 
+                       0.0057646, 0.0046117, 0.0036893, 0.0029515, 0.0023612, 0.0018889, 
+                       0.0015112, 0.0012089, 0.0009671])
+        equil_c = np.array([3158.0, 3449.0, 3599.0, 3654.0, 3723.0, 3576.0, 3408.0, 4653.0, 4722.0])
+        equil_w = ' 0.00000 7.67256 13.88177 16.90285 18.80824 20.54981 21.23185 17.69051 14.88619\n'
+
+        np.testing.assert_array_almost_equal(t2, test_3.get_WL_data()[0], 10)
+        np.testing.assert_array_almost_equal(w2, test_3.get_WL_data()[1], 10)
+        np.testing.assert_array_almost_equal(equil_c, test_3.equil_c, 10)
+        assert test_3.EXE_status == 'equilibrated'
+        assert test_3.equil_t == 2.87516
+        assert test_3.equil_w == equil_w
+        
+
+    
+
 
 
